3fe21396580ccb6d052269887b87322fd6cfb350
Noah Perlmutter <noah.harrison@protonmail.com>
2022-03-04 17:47:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './PerlTech_Simulations_Logo_Red.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>パールテックシミュレーション</h1>
+        <h2>Simulating the best in reality.</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
